555ba7b4c2913fd6b4e7062d57e0686da906cf0e
AlexLeyba <deathnotka@gmail.com>
2018-06-18 22:07:53 +0300

    light#1 and light#2

--- firstapp/views.py
+++ firstapp/views.py
@@ -7,10 +7,10 @@ from firstapp.models import Alex
 # функция заносит данные из формы в бозу данных
 def form_set(request):
     a = request.GET.get('context_value', '')
-    # a_2 = request.GET.get('title_value', '')
-    # a_3 = request.GET.get('slug_value', '')
-    if str(a) != '':  # or str(a_2) != '' or str(a_3) != '':
-        i = Alex.objects.create(context=a)#, title=a_2, slug=a_3)
+    a_2 = request.GET.get('title_value', '')
+    a_3 = request.GET.get('slug_value', '')
+    if str(a) != '' or str(a_2) != '' or str(a_3) != '':
+        i = Alex.objects.create(context=a, title=a_2, slug=a_3)
         i.save()
     return render(request, 'admin1.html')
 
